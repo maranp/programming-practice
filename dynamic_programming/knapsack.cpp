d94405bb3d2a6d54b9c5778b1e25e9e6c60ef810
Maran Pakkirisamy <maran.pakkirisamy@cavium.com>
2018-04-12 17:27:35 +0530

    knapsack started. code incomplete

---
+++ dynamic_programming/knapsack.cpp
@@ -0,0 +1,22 @@
+/*
+ * knapsack.cpp
+ *
+ *  Created on: 12-Apr-2018
+ *      Author: maran
+ */
+
+#include "ihelper.h"
+
+//bool find_config(vector<int> w, )
+
+int main() {
+  vector<int> w {7, 3, 2};
+  auto total = 12;
+  vector<int> config {0, 0, 0};
+
+  //cout << type_print_pretty<decltype(total)>() << endl;
+  //find_config(w, total, config);
+}
+
+
+
